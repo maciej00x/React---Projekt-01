95e17a3cf01815b3f94460c8064effd7429512f4
unknown <m.zawadzki022@gmail.com>
2024-03-31 20:26:13 +0200

    projekt

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,12 +3,16 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="main-header">
-      <div className="assets container main-header-content">
+      <div className="container main-header-content">
         <h1 className="main-header-heading">
           Nasza firma oferuje najwyższej jakości produkty.
         </h1>
         <h2 className="h2-style">Nie wierz nam na słowo - sprawdź</h2>
-        <a href="#our-product" className="button-style">
+        <a
+          Link
+          to="./components/BoxSection/BoxSection"
+          className="button-style"
+        >
           oferta
         </a>
       </div>
